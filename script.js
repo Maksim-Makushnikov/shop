ce8273c513f7d6fb4c90582d5be92d8d78f05a36
Maksim-Makushnikov <85613224+Maksim-Makushnikov@users.noreply.github.com>
2022-06-03 14:35:35 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+        margin:30,
+        nav:true,
+        items:1,
+        dots:false,
+        autoplay:true,
+        smartSpeed:500,
+        autoplayTimeout:5000
+    });
+});
